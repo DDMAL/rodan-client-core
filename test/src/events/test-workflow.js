1bafd936e80f3809e9e7bc8ed5eb3c214bd3dbf7
Michael Ryan Bannon <ryan.bannon@gmail.com>
2017-03-06 15:33:08 -0500

    WRK: unit tests

--- test/src/events/test-workflow.js
+++ test/src/events/test-workflow.js
@@ -1,4 +1,61 @@
+var project = null;
+var workflow = null;
 describe('Workflow', function()
 {
-    it('Workflow events');
+    before(function(done) { setupWorkflow(done); });
+    after(ensureDeinitialization);
+    var requests = [rodan.events.REQUEST__WORKFLOW_CREATE,
+    				rodan.events.REQUEST__WORKFLOW_DELETE,
+    				rodan.events.REQUEST__WORKFLOW_SAVE];
+    describe(requests.join(', '), function()
+    {
+        it(rodan.events.EVENT__WORKFLOW_CREATED, function(done)
+        {
+            rodan.channel.once(rodan.events.EVENT__WORKFLOW_CREATED, function(options)
+            {
+            	workflow = options.workflow;
+            	assert.instanceOf(workflow, rodan.Workflow);
+                done(); 
+            });
+            rodan.channel.request(rodan.events.REQUEST__WORKFLOW_CREATE, {project: project});
+        });
+
+        var randomName = 'newname' + createRandomId().toString();
+        it(rodan.events.EVENT__WORKFLOW_SAVED, function(done)
+        {
+            rodan.channel.once(rodan.events.EVENT__WORKFLOW_SAVED, function(options)
+            {
+            	workflow = options.workflow;
+            	assert.equal(randomName, workflow.get('name'));
+                done(); 
+            });
+            rodan.channel.request(rodan.events.REQUEST__WORKFLOW_SAVE, {workflow: workflow, fields: {name: randomName}});
+        });
+
+        it(rodan.events.REQUEST__WORKFLOW_DELETE, function(done)
+        {
+            rodan.channel.once(rodan.events.EVENT__WORKFLOW_DELETED, function(options)
+            {
+                done(); 
+            });
+            rodan.channel.request(rodan.events.REQUEST__WORKFLOW_DELETE, {workflow: workflow});
+        });
+
+        it(rodan.events.REQUEST__WORKFLOW_EXPORT);
+        it(rodan.events.REQUEST__WORKFLOW_IMPORT);
+    });
 });
+
+function setupWorkflow(done)
+{
+	ensureInitializationAndLogin(function()
+	{
+    	rodan.channel.once(rodan.events.EVENT__PROJECT_CREATED, function(options)
+    	{
+    		project = options.project;
+    		done();
+    	});
+    	var user = rodan.channel.request(rodan.events.REQUEST__AUTHENTICATION_USER);
+        rodan.channel.request(rodan.events.REQUEST__PROJECT_CREATE, {user: user});
+	});
+}
